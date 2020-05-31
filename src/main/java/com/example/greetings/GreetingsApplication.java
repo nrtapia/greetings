f3c26e2f7814525bd2820d03c32293edb3e3ff8b
Neider Tapia <neider.tapia@gmail.com>
2020-05-31 16:59:46 -0500

    first commit

---
+++ src/main/java/com/example/greetings/GreetingsApplication.java
@@ -0,0 +1,44 @@
+package com.example.greetings;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+public class GreetingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingsApplication.class, args);
+    }
+
+}
+
+@RestController
+class Controller {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Greeting {
+
+    private long id;
+    private String content;
+}
